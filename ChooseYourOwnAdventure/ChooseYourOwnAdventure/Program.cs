20500acfb5ce12ce4ce1c94801a61419680e5b9a
Chez <chez.vang@gmail.com>
2017-11-16 09:29:35 -0600

    player alive check is working

--- ChooseYourOwnAdventure/ChooseYourOwnAdventure/Program.cs
+++ ChooseYourOwnAdventure/ChooseYourOwnAdventure/Program.cs
@@ -10,6 +10,17 @@ namespace ChooseYourOwnAdventure
     {
         static void Main(string[] args)
         {
+            //player start, alive
+            StartPlayer startPlayer = new StartPlayer(true);
+            Console.ReadKey();
+            //player goes into story, alive
+            //Story startPlayer = new Story(true);
+            //check if player alive or dead when out of story
+            //if player dead, end, game over
+            
+            //if player alive, end, win
+
+
         }
     }
 }
---
+++ ChooseYourOwnAdventure/ChooseYourOwnAdventure/StartPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourOwnAdventure
+{
+    class StartPlayer
+    {
+        //player alive or dead
+        public bool isPlayerAlive;
+        public string playerStatus;
+
+        public StartPlayer(bool isPlayerAlive)
+        {
+            if (isPlayerAlive == true)
+            {
+                isPlayerAlive = true;
+                Console.WriteLine("The player is alive.");
+            }
+            else
+            {
+                isPlayerAlive = false;
+                Console.WriteLine("The player is dead.");
+            }
+        }
+    }
+}
